e9e115e71508fbd533d739383686a6deafbfc7e7
Wu Xudong <tikiet@gmail.com>
2014-07-25 23:54:37 +0800

    Add user detail header xib file
    Signed-off-by: Wu Xudong <tikiet@gmail.com>

---
+++ Viewport/VPUserDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VPUserDetailViewController.h
+//  Viewport
+//
+//  Created by 吴旭东 on 7/25/14.
+//  Copyright (c) 2014 xudongwu.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VPUserDetailViewController : NSViewController
+
+@end
---
+++ Viewport/VPUserDetailViewController.m
@@ -0,0 +1,26 @@
+//
+//  VPUserDetailViewController.m
+//  Viewport
+//
+//  Created by 吴旭东 on 7/25/14.
+//  Copyright (c) 2014 xudongwu.com. All rights reserved.
+//
+
+#import "VPUserDetailViewController.h"
+
+@interface VPUserDetailViewController ()
+
+@end
+
+@implementation VPUserDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
