44b9479f5b7e9c087506dcda28670577ae4b1abd
Wu Xudong <tikiet@gmail.com>
2014-08-09 14:52:50 +0800

    Add search result user view controller
    Signed-off-by: Wu Xudong <tikiet@gmail.com>

---
+++ Viewport/VPSearchUserResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  VPSearchUserResultViewController.h
+//  Viewport
+//
+//  Created by 吴旭东 on 8/9/14.
+//  Copyright (c) 2014 xudongwu.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VPSearchUserResultViewController : NSViewController
+
+@end
--- Viewport/VPSearchViewController.m
+++ Viewport/VPSearchViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "VPSearchViewController.h"
+#import "VPInfo.h"
+#import "VPConnectionDataDepot.h"
 
 @interface VPSearchViewController ()
 
@@ -19,7 +21,27 @@
 }
 
 - (IBAction)enter:(id)sender {
-    NSLog(@"enter:%@", sender);
+    [self startSearch:((NSSearchField *)sender).stringValue];
 }
 
+- (void) startSearch:(NSString*)query
+{
+    NSURL *url = [VPInfo retrieveUserSearchUrlWithQueryParameter:query];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLConnection *con =
+    [[NSURLConnection alloc] initWithRequest:request
+                                    delegate:[[VPConnectionDataDepot alloc]
+                                              initWithSuccessBlock:^(NSData *data){
+                                                  [self updateData:data];
+                                              } failBlock:^(NSError *error){
+                                                  NSLog(@"error:%@", error);
+                                              }]];
+    [con start];
+}
+
+-(void)updateData:(NSData*)data
+{
+    NSDictionary *raw = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    NSLog(@"raw:%@", raw);
+}
 @end
